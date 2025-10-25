127eaead950e247f40708ed02d86dcc8244f060d
Nicole0509 <niashavril0509@gmail.com>
2025-10-25 08:53:14 +0200

    Created product and category tables in the DB

--- ProductManagement/src/main/java/org/example/productmanagement/Models/Product.java
+++ ProductManagement/src/main/java/org/example/productmanagement/Models/Product.java
@@ -1,9 +1,6 @@
 package org.example.productmanagement.Models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,6 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name = "product")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
