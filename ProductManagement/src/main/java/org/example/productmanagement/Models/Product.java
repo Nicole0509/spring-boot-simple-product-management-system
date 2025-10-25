78ad395a301340285acbaa4a6ee5ea4bfe70b0c9
Nicole0509 <niashavril0509@gmail.com>
2025-10-25 08:03:05 +0200

    Created the product entity class

---
+++ ProductManagement/src/main/java/org/example/productmanagement/Models/Product.java
@@ -0,0 +1,21 @@
+package org.example.productmanagement.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private double price;
+}
