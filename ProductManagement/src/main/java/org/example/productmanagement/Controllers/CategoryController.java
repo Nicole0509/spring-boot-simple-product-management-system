3694858eb9e267bfca6e6bc209b037ecbafb798a
Nicole0509 <niashavril0509@gmail.com>
2025-10-25 11:00:44 +0200

    Created a category successfully

--- ProductManagement/src/main/java/org/example/productmanagement/Controllers/CategoryController.java
+++ ProductManagement/src/main/java/org/example/productmanagement/Controllers/CategoryController.java
@@ -1,13 +1,23 @@
 package org.example.productmanagement.Controllers;
 
+import org.example.productmanagement.DTOs.CategoryDTO;
 import org.example.productmanagement.Services.CategoryService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/category")
 public class CategoryController {
     private final CategoryService categoryService;
 
     public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
+    }
+
+    @PostMapping
+    public CategoryDTO createCategory(@RequestBody CategoryDTO categoryDTO){
+        return categoryService.createCategory(categoryDTO);
     }
 }
--- ProductManagement/src/main/java/org/example/productmanagement/Services/CategoryService.java
+++ ProductManagement/src/main/java/org/example/productmanagement/Services/CategoryService.java
@@ -1,5 +1,7 @@
 package org.example.productmanagement.Services;
 
+import org.example.productmanagement.DTOs.CategoryDTO;
+import org.example.productmanagement.Models.Category;
 import org.example.productmanagement.Repositories.CategoryRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +11,14 @@ public class CategoryService {
 
     public CategoryService(CategoryRepository categoryRepo) {
         this.categoryRepo = categoryRepo;
+    }
+
+    public CategoryDTO createCategory(CategoryDTO categoryDTO) {
+        Category category = new Category();
+        category.setName(categoryDTO.getName());
+        category.setDescription(categoryDTO.getDescription());
+        categoryRepo.save(category);
+
+        return categoryDTO;
     }
 }
